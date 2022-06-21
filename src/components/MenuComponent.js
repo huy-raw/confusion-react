e6d97d092315e1c6b7daaab9a7d8532e26d049d4
huy-raw <ruacon130@gmail.com>
2022-06-21 09:48:50 +0700

    fix:react router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import DishDetail from "./DishDetailComponent"
+import React, { useState } from 'react';
 
-
-
-
-
-function RenderMenuItem({dish, onClick}) {
+function RenderMenuItem({ dish, onClick }) {
     return (
-        <Card onClick={()=> onClick(dish.id)}>
-            <CardImg with="100%" src={dish.image} alt={dish.name}/>
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
-    
-    );
-       
-}
-
-const Menu = (props) => {
-    const menu = props.dishes.map((dish) => {
-        return (
-            <div className="col-12 col-md-5 m-1" key={dish.id}>
-                <RenderMenuItem dish={dish} onClick={props.onClick}/>
-            </div>
-        );
-    });
-
-
-    return (
-        <div className="container">
-            <div className="row">{menu}</div>
+        <div className="col-12 col-md-5 m-1" onClick={() => onClick(dish.id)}  >
+            <Card >
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
         </div>
     )
-    
 }
 
-export default Menu
+
+const Menu = (props) => {
+    const [selectedDish, setSelectedDish] = useState(null);
+    const handleShowDetail = (dish)=>{
+        setSelectedDish(dish)
+    }
+    const menu = props.dishes.map(dish => {
+        return (
+            <RenderMenuItem key={dish.id} dish={dish} onClick={()=>handleShowDetail(dish)} ></RenderMenuItem>
+        )
+    })
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+            <DishDetail dish={selectedDish}></DishDetail>
+        </>
+
+    )
+}
+
+
+export default Menu;
